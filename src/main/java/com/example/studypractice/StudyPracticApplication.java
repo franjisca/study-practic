aa1cac0c5e115497910200bf8e1e438d05cb1bdf
JIYEON <publicjy324@gmail.com>
2023-10-23 10:47:13 +0900

    changed name

--- src/main/java/com/example/studypractice/StudyPracticApplication.java
+++ src/main/java/com/example/studypractice/StudyPracticApplication.java
@@ -1,4 +1,4 @@
-package com.example.studypractic;
+package com.example.studypractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
