3b88474bff0bfa3adfbe5ff03e7a1dc33bceb56d
JIYEON <publicjy324@gmail.com>
2023-10-23 10:41:03 +0900

    Initial commit

---
+++ src/main/java/com/example/studypractic/StudyPracticApplication.java
@@ -0,0 +1,13 @@
+package com.example.studypractic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPracticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyPracticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studypractic/StudyPracticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studypractic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPracticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
